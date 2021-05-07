f56c43915bb9e10012d41967117bc1659d3de79b
pkm774 <mprabhat774@gmail.com>
2021-05-07 07:31:35 +0600

    sdm845-common: Build graphics.allocator 3.0 and 4.0
    565 I hwservicemanager: getTransport: Cannot find entry android.hardware.graphics.allocator@4.0::IAllocator/default in either framework or device manifest.
    4126 W Gralloc4: allocator 3.x is not supported
    565 I hwservicemanager: getTransport: Cannot find entry android.hardware.graphics.allocator@3.0::IAllocator/default in either framework or device manifest.
    4126 W Gralloc3: allocator 3.x is not supported
    4126 I Gralloc2: Adding additional valid usage bits: 0x0
    
    Change-Id: I334d92b274101390b782ac82bad162ac12b2ca4b
    Signed-off-by: krtonia <krtonia@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -145,6 +145,8 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@3.0-impl \
+    android.hardware.graphics.allocator@4.0-impl \
     android.hardware.graphics.composer@2.3-service \
     android.hardware.graphics.mapper@2.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
