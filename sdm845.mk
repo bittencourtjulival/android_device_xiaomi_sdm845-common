412d212fea01c9ab1f6fbce62d0d33032f7fe8e9
spkal01 <kalligeross@gmail.com>
2022-04-27 20:40:38 +0300

    sdm845-common: overlays: Add support for smart charging and battery health

--- sdm845.mk
+++ sdm845.mk
@@ -15,6 +15,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-spark
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
